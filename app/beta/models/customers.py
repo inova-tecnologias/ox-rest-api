d6cf362663b1e7b1cd2f9bd246de37f2986185a9
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2020-07-23 16:39:15 -0300

    Modfied customers fields

--- app/beta/models/customers.py
+++ app/beta/models/customers.py
@@ -11,20 +11,23 @@ context_model = Context.resource_model
 
 class Customer(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(30))
-    description = db.Column(db.String(200))
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(256))
+    cnpj = db.Column(db.String(20))
     users = db.relationship('User', backref='customer', lazy=True)
     contexts = db.relationship('Context', backref='customer', lazy=True)
 
 
     register_model = api.model('Register Customer', {
     'name': fields.String(required=True),
+    'cnpj': fields.String(required=True),
     'description': fields.String()
     })
     
     resource_model = api.model('Customer', {
     'id': fields.Integer(),
     'name': fields.String(),
+    'cnpj': fields.String(),
     'description': fields.String(),
     'users': fields.Nested(user_model),
     'contexts': fields.Nested(context_model)
