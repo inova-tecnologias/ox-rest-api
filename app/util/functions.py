6f1a07a2246882f1903255b51551c29cd5ac326d
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2020-08-07 08:55:09 -0300

    Added password func

---
+++ app/util/functions.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def random_pass(length=25):
+    # Random string with the combination of lower and upper case
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
