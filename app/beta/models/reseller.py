87954aaa5f778bbbe539c1b8be1cbdeeed87f173
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2020-09-21 18:18:22 -0300

    Added reseller object

--- app/beta/models/reseller.py
+++ app/beta/models/reseller.py
@@ -3,33 +3,32 @@ from flask_restplus import fields
 from app import db
 from .. import api
 from ..models.users import User
-from ..models.contexts import Context
+from ..models.customer import customer
 
 
 user_model = User.resource_model
-context_model = Context.resource_model
+customer_model = Customer.resource_model
 
-class Customer(db.Model):
+class Reseller(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String(256))
     cnpj = db.Column(db.String(20))
-    users = db.relationship('User', backref='customer', lazy=True)
-    contexts = db.relationship('Context', backref='customer', lazy=True)
+    users = db.relationship('User', backref='reseller', lazy=True)
+    customer = db.relationship('Customer', backref='reseller', lazy=True)
 
-    reseller_id = db.Column(db.Integer, db.ForeignKey('reseller.id'))    
 
-    register_model = api.model('Register Customer', {
+    register_model = api.model('Register Reseller', {
     'name': fields.String(required=True),
     'cnpj': fields.String(required=True),
     'description': fields.String()
     })
     
-    resource_model = api.model('Customer', {
+    resource_model = api.model('Reseller', {
     'id': fields.Integer(),
     'name': fields.String(),
     'cnpj': fields.String(),
     'description': fields.String(),
     'users': fields.Nested(user_model),
-    'contexts': fields.Nested(context_model)
+    'reseller': fields.Nested(context_model)
     }) 
